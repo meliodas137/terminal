e8d244826d10a395723f6d2449a898f9240272a5
meliodas137 <mayankgt1043@gmail.com>
2023-02-16 14:52:49 -0500

    handled cd and exit

--- executor.c
+++ executor.c
@@ -6,12 +6,31 @@ void executeCmd(char* cmd){
     if(validateCmd(cmdMap)) {
         runCmd(cmdMap);
     } else {
-        printf("Error: invalid command\n");
-        return;
+        fprintf(stderr, "Error: invalid command\n");
     }
+    free(cmdMap);
+    return;
 }
 
 int runCmd(char** cmdMap){
-    if(cmdMap) return 1;
+    if(cmdMap[0] == NULL) return 1;
+    if(strcmp(cmdMap[0], "cd") == 0 || strcmp(cmdMap[0], "exit") == 0 
+        || strcmp(cmdMap[0], "fg") == 0 || strcmp(cmdMap[0], "jobs") == 0) handleBuiltInCmd(cmdMap);
+    return 0;
+}
+
+int handleBuiltInCmd(char** cmdMap) {
+    if(strcmp(cmdMap[0], "exit") == 0) {
+        exit(0);
+    }
+    // if(strcmp(cmdMap[0], "jobs") == 0) {
+    //     jobs();
+    // }
+    // if(strcmp(cmdMap[0], "fg")) {
+    //     fg(cmdMap[1]);
+    // }
+    if(strcmp(cmdMap[0], "cd") == 0) {
+        chdir(cmdMap[1]);
+    }
     return 0;
 }
--- executor.h
+++ executor.h
@@ -1,7 +1,10 @@
+#include <stdio.h>
+
 #ifndef _EXECUTOR_H_
 #define _EXECUTOR_H_
 
 void executeCmd(char* cmd);
 int runCmd(char** cmdMap);
+int handleBuiltInCmd(char** cmdMap);
 
 #endif
