86a9f224127b8501c454b09a8f99cf9bbc112036
meliodas137 <mayankgt1043@gmail.com>
2023-02-17 20:01:16 -0500

    added basic signal handler for terminal

--- nyush.c
+++ nyush.c
@@ -6,12 +6,16 @@ https://www.w3schools.com/c/c_strings.php
 https://www.asciitable.com/
 https://pubs.opengroup.org/onlinepubs/009695399/functions/exit.html
 https://man7.org/linux/man-pages/man3/errno.3.html
+https://linuxhint.com/signal_handlers_c_programming_language/
 Class Slides
 */
 #include "executor.h"
 #include "cmdutils.h"
 
 int main(){
+    signal(SIGTSTP, SIG_IGN);
+    signal(SIGINT, SIG_IGN);
+    signal(SIGQUIT, SIG_IGN);
     char* cmd = malloc(1000);
     char* cwdBuffer = malloc(1000);
     char* dirName = getcurDir(cwdBuffer);
