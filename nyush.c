b46f1e24db6288a6ce3985215bc89c80e6f5f2e9
meliodas137 <mayankgt1043@gmail.com>
2023-02-27 13:07:01 -0500

    added ref

--- nyush.c
+++ nyush.c
@@ -7,6 +7,7 @@ https://www.asciitable.com/
 https://pubs.opengroup.org/onlinepubs/009695399/functions/exit.html
 https://man7.org/linux/man-pages/man3/errno.3.html
 https://linuxhint.com/signal_handlers_c_programming_language/
+https://man7.org/linux/man-pages/man2/pipe.2.html
 Class Slides
 */
 #include "executor.h"
